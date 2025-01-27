89a044b4ea588f677e6a96eff625a6ac402b4698
Sam Machin <sam@sammachin.com>
2025-01-27 12:27:09 +0000

    change port env var

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const {createEndpoint} = require('@jambonz/node-client-ws');
 const server = createServer();
 const makeService = createEndpoint({server});
 const logger = require('pino')({level: process.env.LOGLEVEL || 'info'});
-const port = process.env.WS_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 require('./lib/routes')({logger, makeService});
 
